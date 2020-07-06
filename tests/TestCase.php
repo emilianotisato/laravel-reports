81ce3e8ebedaae1d00678f0d221287842caf805e
emilianotisato <emilianotisato@users.noreply.github.com>
2020-07-06 22:53:25 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Emilianotisato\LaravelReport\Tests;
 
-use Orchestra\Testbench\TestCase as Orchestra;
 use Emilianotisato\LaravelReport\LaravelReportServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
